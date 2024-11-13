1cd82e88639430a4e7f4deb2e90798801c7abd4e
b.endres <endres@systopia.de>
2024-11-13 17:09:12 +0100

    [WIP] test-driven development

--- tests/phpunit/NodePeeringTest.php
+++ tests/phpunit/NodePeeringTest.php
@@ -4,7 +4,9 @@ use Civi\Test\HeadlessInterface;
 use Civi\Test\HookInterface;
 use Civi\Test\TransactionalInterface;
 
-//use CRM_Share_ExtensionUtil as E;
+use CRM_Share_ExtensionUtil as E;
+
+
 
 /**
  * FIXME - Add test description.
@@ -34,26 +36,78 @@ class NodePeeringTest extends \PHPUnit\Framework\TestCase implements HeadlessInt
    * @throws \CRM_Extension_Exception_ParseException
    */
   public function setUpHeadless() {
-
-    // drop tables - @todo remove
-    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS civicrm_share_node");
-    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS civicrm_share_node_peering");
-    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS civicrm_share_change");
-    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS civicrm_share_handler");
+//
+//
+//    // delete tables
+//    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS civicrm_share_change");
+//    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS  civicrm_share_handler");
+//    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS  civicrm_share_node_peering");
+//    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS  civicrm_share_node");
 
     return \Civi\Test::headless()
-//      ->install(['something'])
       ->installMe(__DIR__)
       ->apply();
+  }
+
+  public function setUp(): void
+  {
+    // clean tables
+    CRM_Core_DAO::executeQuery("DELETE FROM civicrm_share_change");
+    CRM_Core_DAO::executeQuery("DELETE FROM civicrm_share_handler");
+    CRM_Core_DAO::executeQuery("DELETE FROM civicrm_share_node_peering");
+    CRM_Core_DAO::executeQuery("DELETE FROM civicrm_share_node");
+
+    parent::setUp();
   }
 
 
   /**
    * This will manually set up a peering ON THE SAME SYSTEM
    */
+  public function testBasicSetup():void {
+    // create a local node
+    $local_node = \Civi\Api4\ShareNode::create(false)
+      ->addValue('name', 'Local Node 1')
+      ->addValue('short_name', 'basic_01')
+      ->addValue('is_local', true)
+      ->addValue('description', "automated test node")
+      ->addValue('rest_url', 'TODO')
+      ->addValue('api_key', 'TODO')
+      ->addValue('auth_key', 'TODO')
+      ->addValue('is_enabled', true)
+      ->addValue('receive_identifiers', CRM_Utils_Array::implodePadded(''))
+      ->addValue('send_identifiers', '')
+      ->execute()
+      ->first();
+
+
+    // create a "remote" node
+    $remote_node = \Civi\Api4\ShareNode::create(false)
+      ->addValue('name', 'basic_02_remote')
+      ->addValue('short_name', 'Basic "Remote" Node')
+      ->addValue('is_local', false)
+      ->execute()
+      ->first();
+
+    // create a peering (cheekily
+    $shared_key = base64_encode(random_bytes(64));
+    $node_peering = \Civi\Api4\ShareNodePeering::create(TRUE)
+      ->addValue('local_node', $local_node['id'])
+      ->addValue('remote_node', $remote_node['id'])
+      ->addValue('is_enabled', true)
+      ->addValue('shared_secret', $shared_key)
+      ->execute();
+
+
+    $this->assertTrue(true);
+
+  }
+
+  /**
+   * This will manually set up a peering ON THE SAME SYSTEM
+   */
   public function testPeeringSetup():void {
     // create a local node
-
     $this->assertTrue(true);
   }
 
