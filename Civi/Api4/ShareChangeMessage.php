f3fe5d38e33ebcda6b96e74d04c07d25a6ab542b
Jens Schuppe <schuppe@systopia.de>
2024-12-12 15:10:31 +0100

    Add default and meta permissions for ShareChangeMessage.receive API

--- Civi/Api4/ShareChangeMessage.php
+++ Civi/Api4/ShareChangeMessage.php
@@ -30,6 +30,8 @@ class ShareChangeMessage extends Generic\AbstractEntity {
    */
   public static function permissions() {
     return [
+      'meta' => ['access CiviCRM'],
+      'default' => ['administer CiviCRM'],
       'receive' => [Permissions::RECEIVE_CHANGE_MESSAGES],
     ];
   }
